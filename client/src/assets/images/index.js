dcbbb6ad2f50e033d00e5e8c8fd734ec80e03b12
Abdul Wasay Abbas <abdulwasay@devflovv.com>
2023-12-21 22:12:14 +0500

    Upload Screen

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -1,3 +1,4 @@
 export const appLogo = require('./appLogo.svg');
 export const appImage = require('./appLogo.png');
 export const checkBoxOutline = require('./checkBoxOutline.png');
+export const uploadVideoImage = require('./Videos.png');
