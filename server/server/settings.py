f8bc643479e7981331d5071c933cea7918d7d17c
ahmaddevflovv <117106826+ahmaddevflovv@users.noreply.github.com>
2023-12-28 18:40:44 +0500

    removed comented code from setting file

--- server/server/settings.py
+++ server/server/settings.py
@@ -30,19 +30,10 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = ["http://localhost:3000", "http://localhost:8000", "http://localhost"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = ['*']
-
-
-# CORS_ALLOW_CREDENTIALS = False
-# CORS_ORIGIN_WHITELIST = [
-#     "http://localhost:3000",
-#     "http://localhost:8000",
-#     "http://localhost"
-# ]
 
 # Application definition
 
