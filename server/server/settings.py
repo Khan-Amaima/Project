0c0a9b5ff24213e7770e49f11d47456465822a1f
ahmaddevflovv <117106826+ahmaddevflovv@users.noreply.github.com>
2023-12-28 18:08:49 +0500

    updated placement of middleware

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,7 +37,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = ['*']
 
 
-CORS_ALLOW_CREDENTIALS = False
+# CORS_ALLOW_CREDENTIALS = False
 # CORS_ORIGIN_WHITELIST = [
 #     "http://localhost:3000",
 #     "http://localhost:8000",
@@ -59,10 +59,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
