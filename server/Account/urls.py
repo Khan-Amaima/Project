af31a08566c896585ce27b66b491c1148cbb7dff
ahmaddevflovv <117106826+ahmaddevflovv@users.noreply.github.com>
2023-12-29 01:04:26 +0500

    updated files for auth token

--- server/Account/urls.py
+++ server/Account/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 from .views import UserSignupView
 from .views import UserLoginView
+from .views import UserLogoutView
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('user-signup', UserSignupView.as_view()),
     path("user-login", UserLoginView.as_view()),
+    path("user-logout", UserLogoutView.as_view()),
 ]
