221eec35d98af7f12a2b3659881532bca47c4a64
ahmaddevflovv <117106826+ahmaddevflovv@users.noreply.github.com>
2023-12-30 01:14:54 +0500

    code refactored and created user logout and get_detail API

--- server/Account/views.py
+++ server/Account/views.py
@@ -10,58 +10,82 @@ from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
-from .serializers import UserProfileSerializer, UserSerializer
+from Account.serializers  import UserProfileSerializer, UserGETSerializer
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import logout
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate
 
 def index(request):
     return HttpResponse("Hello, You're at the Account index.")
 
 class UserSignupView(generics.CreateAPIView):
-  permission_classes = (AllowAny,)
-  serializer_class = UserProfileSerializer
+    permission_classes = (AllowAny,)
+        
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = User.objects.filter(email=email).first()
+        if user:
+            return Response({'message' : 'User already exists.'}, status=status.HTTP_400_BAD_REQUEST)
 
-  def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        token, created = Token.objects.get_or_create(user=serializer.instance)
-        return Response({'token': token.key, 'user-info' : {'username' : request.data['username'], 'email' : request.data['email']}}, status=status.HTTP_201_CREATED, headers=headers)
+        username = email.split("@") 
+        hashed_password = make_password(password)
+        user = User.objects.create(
+            username=username[0],
+            email=email,
+            password=hashed_password
+        )
+
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserGETSerializer(user)
+        return Response({'token': token.key, 'user-info' : serializer.data}, status=status.HTTP_201_CREATED,)
   
 class UserLoginView(generics.CreateAPIView):
-    permission_classes = (permissions.AllowAny,)
-    serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = (AllowAny,)
 
-    def post(self, request, format=None):
-        serializer = self.get_serializer(data=self.request.data,
-            context={ 'request': self.request })
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'user-info' : {'username' : request.data['username']}}, status=status.HTTP_202_ACCEPTED)
+    def post(self, request):
+
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not email or not password:
+            return Response({'message' : 'Email and password is required!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.filter(email=email).first()
+        if user:
+            username = user.username
+            auth_user = authenticate(username=username, password=password)
+
+            if auth_user:
+                token, created = Token.objects.get_or_create(user=user)
+                serializer = UserGETSerializer(user)
+                return Response({'token': token.key, 'user-info': serializer.data}, status=status.HTTP_200_OK)
+            else:
+                # Authentication failed
+                msg = 'Access denied: wrong email or password.'
+                return Response({'message': msg}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'message' : 'User not found!'}, status=status.HTTP_400_BAD_REQUEST)
     
 class UserLogoutView(APIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated] 
-
+    permission_classes = [IsAuthenticated]
     def post(self, request):
-        user_token = Token.objects.get(user=request.user)
-        user_token.delete()        
-
+        user_token = Token.objects.filter(user=request.user).first()
+        user_token.delete()
         return Response({"success": ("Successfully logged out.")}, status=status.HTTP_200_OK)
+
 
 class UserDetailView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated] 
 
     def get(self, request):
+        user = request.user
+        serializer = UserGETSerializer(user)
+        return Response({'user-info' : serializer.data}, status=status.HTTP_200_OK)
 
-        try:
-            user_token = Token.objects.get(user=request.user)
-            serializer = UserProfileSerializer(request.user, many=False)
-            return Response({'user-info' : {'username' : serializer.data['username'], 'email' : serializer.data['email']}}, status=status.HTTP_202_ACCEPTED)
-
-        except Token.DoesNotExist:
-            return Response({'error': 'Token does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        
