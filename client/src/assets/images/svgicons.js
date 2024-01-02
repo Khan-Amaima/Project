42bfdae3e855da40c2b4f02a729bb3da7669a3a8
Abdul Wasay Abbas <abdulwasay@devflovv.com>
2024-01-02 18:51:40 +0500

    Page Not Found Added

--- client/src/assets/images/svgicons.js
+++ client/src/assets/images/svgicons.js
@@ -1,12 +1,14 @@
 import { ReactComponent as AppLogo } from "./appLogo.svg";
 import { ReactComponent as VideoIcon } from "./Videos.svg";
 import { ReactComponent as UploadIcon } from "./Upload-Icon.svg";
-import { ReactComponent as UploadPicIcon } from "./Frame.svg";
+import { ReactComponent as UploadPicIcon } from "./UploadPicture.svg";
 import {ReactComponent as DragIcon } from './Drag.svg'
 import {ReactComponent as LogOutIcon } from './Logout.svg'
 import {ReactComponent as DeleteIcon } from './Delete.svg'
 import {ReactComponent as SoundIcon } from './Sound.svg'
 import {ReactComponent as MuteSoundIcon } from './MuteSound.svg'
+import {ReactComponent as PageNotFound } from './Page404.svg'
+
 const SvgIcons = {
   appLogo: <AppLogo />,
   dragIcon: <DragIcon/>,
@@ -17,7 +19,7 @@ const SvgIcons = {
   uploadIcon: <UploadIcon />,
   uploadPicIcon: <UploadPicIcon/>,
   videoIcon: <VideoIcon />,
- 
+  pageNotFound: <PageNotFound />,
   
 };
 
