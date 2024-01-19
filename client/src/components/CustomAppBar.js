bc8b80dc3e97cd0dba74c269df497688dcb48f8e
Abdul Wasay Abbas <abdulwasay@devflovv.com>
2024-01-19 21:49:30 +0500

    searchBar Width  and text inline Height

--- client/src/components/CustomAppBar.js
+++ client/src/components/CustomAppBar.js
@@ -68,9 +68,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
     width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
+    // minWidth:"200px"
   },
 }));
 
@@ -270,7 +268,7 @@ function CustomAppBar({ toggleDrawer, open }) {
             {getCurrentLocation(location)}
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
-          <Search>
+          <Search style={{minWidth:"200px"}}>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
